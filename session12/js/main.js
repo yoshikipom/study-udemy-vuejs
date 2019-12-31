c3952777217f3ff799ae45d1f505610fdcf8f7f6
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2019-12-31 15:11:59 +0900

    [Add] form bindings

---
+++ session12/js/main.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: "hello world",
+    checked: false,
+    colors: [],
+    color: "",
+    selected: "",
+    selectedList: [],
+    lazyMessage: "",
+    trimMessage: "",
+    age: 0,
+  },
+  methods: {
+  },
+})
